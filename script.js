bdc769dff044cecde15460eed941f486c35bf810
adarsh25t <adarsh25t@gmail.com>
2021-11-24 20:04:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     myBtn.style.display = "block";
+//     Header.classList.add("header");
+//   } else {
+//     myBtn.style.display = "none";
+//     Header.classList.remove("header");
+//   }
+// }
+
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+// myBtn.addEventListener('click',topFunction)
+
+
